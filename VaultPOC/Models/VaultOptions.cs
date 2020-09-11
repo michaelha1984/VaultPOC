16f17644b205847ac1070c7c507090870cef1cc5
mha <michael.ha@ivegroup.com.au>
2020-09-11 11:38:31 +1000

    Add validation.

--- VaultPOC/Models/VaultOptions.cs
+++ VaultPOC/Models/VaultOptions.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace VaultPOC.Models
 {
     public class VaultOptions
     {
         public const string Vault = "Vault";
+
+        [Url(ErrorMessage = "Invalid Vault URL")]
         public string Url { get; set; }
+
         public string Name { get; set; }
         public string Token { get; set; }
     }
